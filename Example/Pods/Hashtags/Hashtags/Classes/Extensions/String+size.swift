fa79f619912f817ab09d73215e5defeb4b8bb1ac
Pooja Gupta <pooja.g@simformsolutions.com>
2021-06-07 21:45:30 +0530

    Feature: Multiline support of added tags.

---
+++ Example/Pods/Hashtags/Hashtags/Classes/Extensions/String+size.swift
@@ -0,0 +1,42 @@
+//
+//  String+size.swift
+//  Outfit
+//
+//  Created by Oscar Götting on 5/11/18.
+//  Copyright © 2018 Oscar Götting. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension String {
+    
+    func widthOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.width
+    }
+    
+    func heightOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.height
+    }
+    
+    func sizeOfString(usingFont font: UIFont) -> CGSize {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        return self.size(withAttributes: fontAttributes)
+    }
+    
+    func heightForView(font:UIFont, width:CGFloat) -> CGFloat {
+        let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: CGFloat.greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.font = font
+        label.text = self
+        
+        label.sizeToFit()
+        return label.frame.height
+    }
+
+}
---
+++ Example/Pods/Hashtags/Hashtags/Classes/HashtagCollectionViewCell.swift
@@ -0,0 +1,81 @@
+//
+//  HashtagCollectionViewCell.swift
+//  Hashtags
+//
+//  Created by Oscar Götting on 6/8/18.
+//  Copyright © 2018 Oscar Götting. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class HashtagCollectionViewCell: UICollectionViewCell {
+    
+    static let cellIdentifier = "HashtagCollectionViewCell"
+    
+    var paddingLeftConstraint: NSLayoutConstraint?
+    var paddingRightConstraint: NSLayoutConstraint?
+    var paddingTopConstraint: NSLayoutConstraint?
+    var paddingBottomConstraint: NSLayoutConstraint?
+    
+    lazy var wordLabel : UILabel = {
+        let lbl = UILabel()
+        lbl.textColor = UIColor.white
+        lbl.textAlignment = .center
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.numberOfLines = 0
+        return lbl
+    }()
+    
+    open var hashtag: HashTag?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    open func setup() {
+        self.clipsToBounds = true
+        
+        self.addSubview(wordLabel)
+        
+        // Padding left
+        self.paddingLeftConstraint = self.wordLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor)
+        self.paddingLeftConstraint!.isActive = true
+        // Padding top
+        self.paddingTopConstraint = self.wordLabel.topAnchor.constraint(equalTo: self.topAnchor)
+        self.paddingTopConstraint!.isActive = true
+        // Padding bottom
+        self.paddingBottomConstraint = self.wordLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        self.paddingBottomConstraint!.isActive = true
+        // Padding right
+        self.paddingRightConstraint = self.wordLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor)
+        self.paddingRightConstraint!.isActive = true
+    }
+    
+    open override func prepareForInterfaceBuilder() {
+        self.wordLabel.text = ""
+        super.prepareForInterfaceBuilder()
+    }
+    
+    open func configureWithTag(tag: HashTag, configuration: HashtagConfiguration) {
+        self.hashtag = tag
+        wordLabel.text = tag.text
+        
+        self.paddingLeftConstraint!.constant = configuration.paddingLeft
+        self.paddingTopConstraint!.constant = configuration.paddingTop
+        self.paddingBottomConstraint!.constant = -1 * configuration.paddingBottom
+        self.paddingRightConstraint!.constant = -1 * configuration.paddingRight
+        
+        self.backgroundColor = configuration.backgroundColor
+        
+        self.wordLabel.textColor = tag.isGoldTag ? configuration.goldTagColor : configuration.textColor
+        self.wordLabel.font = configuration.hashtagFont
+        self.wordLabel.layoutSubviews()
+    }
+}
